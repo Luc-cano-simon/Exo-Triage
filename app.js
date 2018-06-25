098d70ab2a877025ab60ad7e01915073d5af7339
luc <luc.cano@hotmail.fr>
2018-06-25 12:15:21 +0200

    Debut de code

---
+++ app.js
@@ -0,0 +1,6 @@
+$('#Group').on('click', function() {
+	$('#Group').html("");
+
+
+var table = ['table 0', 'table 1', 'table 2', 'table 3', 'table 4'];
+var groupe = ["Jonathan", "Luc", "Adeline", "Marc", "Alexandra", "Marco", "Houssine", "Dylan", "Benjamin", "Helder", "Zakia", "Max", "Nicolas", "Nathan", "Dania"
